15503aebfd937e6e9eb468b03fb389ac8a8ef7bb
lans98 <lans9831@gmail.com>
2018-05-18 08:04:33 -0500

    added lokta-volterra simulations - using euler method - using runge-kutta 4th order

--- src/main.rs
+++ src/main.rs
@@ -247,7 +247,7 @@ fn spring_real_simulation() {
   figure.draw().expect("Failed to plot info");
 }
 
-fn lotka_volterra_simulation() {
+fn lotka_volterra_euler_simulation() {
   eprint!("initial poblation A, initial poblation B: ");
   let pa: f64 = scan!();
   let pb: f64 = scan!();
@@ -273,7 +273,68 @@ fn lotka_volterra_simulation() {
   let path = Path::new(&pobb_file);
   let mut pobb_file = File::create(path).unwrap();
 
-  let data = lotka_volterra::dam_predator(pa, pb, a, b, c, d, t0, tf, n);
+  let data = lotka_volterra::dam_predator_with_euler(pa, pb, a, b, c, d, t0, tf, n);
+
+  let mut time = Vec::new();
+  let mut poba = Vec::new();
+  let mut pobb = Vec::new();
+
+  for d in &data {
+    time.push(d.time);
+    poba.push(d.pa);
+    pobb.push(d.pb);
+    poba_file.write_fmt(format_args!("{:.6} {:.6}\n", d.time, d.pa)).unwrap();
+    pobb_file.write_fmt(format_args!("{:.6} {:.6}\n", d.time, d.pb)).unwrap();
+  }
+
+  let mut figure = Figure::new();
+  figure.configure(Key, | k | {
+    k.set(Boxed::Yes);
+    k.set(Position::Inside(Vertical::Top, Horizontal::Left))
+  });
+
+  figure.plot(Lines { x: time.clone(), y: poba.clone() }, | lp | {
+    lp.set(Color::Cyan);
+    lp.set(Label("poblation a"));
+    lp.set(LineType::Dash)
+  });
+
+  figure.plot(Lines { x: time.clone(), y: pobb.clone() }, | lp | {
+    lp.set(Color::Gray);
+    lp.set(Label("poblation b"));
+    lp.set(LineType::Dash)
+  });
+
+  figure.draw().expect("Failed to plot info");
+}
+
+fn lotka_volterra_rkutta_simulation() {
+  eprint!("initial poblation A, initial poblation B: ");
+  let pa: f64 = scan!();
+  let pb: f64 = scan!();
+
+  eprint!("a, b, c, d: ");
+  let a: f64 = scan!();
+  let b: f64 = scan!();
+  let c: f64 = scan!();
+  let d: f64 = scan!();
+
+  eprint!("initial time, final time, number of interactions: ");
+  let t0: f64 = scan!();
+  let tf: f64 = scan!();
+  let n: usize = scan!();
+
+  eprint!("poblation a file, poblation b file: ");
+  let poba_file: String = scan!();
+  let pobb_file: String = scan!();
+
+  let path = Path::new(&poba_file);
+  let mut poba_file = File::create(path).unwrap();
+
+  let path = Path::new(&pobb_file);
+  let mut pobb_file = File::create(path).unwrap();
+
+  let data = lotka_volterra::dam_predator_with_rkutta(pa, pb, a, b, c, d, t0, tf, n);
 
   let mut time = Vec::new();
   let mut poba = Vec::new();
@@ -314,7 +375,8 @@ fn main() {
     println!("2) Pendulum with air resistance");
     println!("3) Spring without cushion");
     println!("4) Spring with cushion");
-    println!("5) Lotka-Volterra predator dam");
+    println!("5) Lotka-Volterra predator dam (euler method)");
+    println!("6) Lotka-Volterra predator dam (runge-kutta 4th order method)");
     print!("Option: ");
     let opt: i32 = scan!();
     match opt {
@@ -322,7 +384,8 @@ fn main() {
         2 => pendulum_real_simulation(),
         3 => spring_ideal_simulation(),
         4 => spring_real_simulation(),
-        5 => lotka_volterra_simulation(),
+        5 => lotka_volterra_euler_simulation(),
+        6 => lotka_volterra_rkutta_simulation(),
         _ => { panic!("Unknown option!!") }
     }
 }
